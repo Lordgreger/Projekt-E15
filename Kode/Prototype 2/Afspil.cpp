b69b12bdbbed545cd4a3d313495e47fec2639503
ReneTHaagensen <cyttie@gmail.com>
2015-12-09 18:02:10 +0100

    afspil & tone opdateringer

--- Kode/Prototype 2/Afspil.cpp
+++ Kode/Prototype 2/Afspil.cpp
@@ -33,8 +33,8 @@ void Afspil::makeRaw0(std::vector<int> input)
 		for (int i = 0; i < ((dtmfToner[input[k]]->getRaw()).size()); i++)
 			raw0.push_back((dtmfToner[input[k]]->getRaw())[i]);
 			
-		/*for (size_t i = 0; i < ((dtmfToner[input[k]]->getRaw()).size()); i++)
-			raw0.push_back((dtmfToner[16]->getRaw())[i]);*/
+		for (size_t i = 0; i < 400; i++)
+			raw0.push_back((dtmfToner[16]->getRaw())[i]);
 	}
 }
 
@@ -42,7 +42,7 @@ int Afspil::afspilToner(int længdeAfElementer)
 {
 	sf::SoundBuffer Buffer;						//Vi opretter et Buffer objekt
 	if (!Buffer.loadFromSamples					//Bufferen loades med vores sinus kurve, antalet af samples, 
-		(&raw0[0], længdeAfElementer * SAMPLES, 1, SAMPLE_RATE))		//kanaler (1 = mono), og sample rate
+		(&raw0[0], længdeAfElementer * SAMPLES + længdeAfElementer*400, 1, SAMPLE_RATE))		//kanaler (1 = mono), og sample rate
 	{
 		std::cerr << "Loading failed!" << std::endl;
 		return 1;								//Siger der er fejl hvis det ikke kan lade sig gøre
